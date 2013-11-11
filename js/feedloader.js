c8597a98b17943b20a4dc87d0bcb68e69451fcc7
Corey Pein <corey+github@coreypein.net>
2013-11-11 13:25:13 +0000

    lots of new things

--- js/feedloader.js
+++ js/feedloader.js
@@ -5,7 +5,7 @@ function OnLoad() {
 var feedControl = new google.feeds.FeedControl();
 
 // Add two feeds.
-feedControl.addFeed("http://realpolitikreport.com/tt-rss/public.php?op=rss&id=-3&key=b72797e36bcc9bcac7d1351efa0d6eecaa81df42", "Reporters' Registry");
+feedControl.addFeed("http://nonoisenews.com/buriedleads.rss", "");
 // feedControl.addFeed("https://feeds.pinboard.in/rss/secret:c4266ce4667c9eca82af/u:warisbusiness/t:wib", "Latest Headlines");
 
 // Draw it.
